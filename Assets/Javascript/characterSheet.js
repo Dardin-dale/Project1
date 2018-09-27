5ff95dda73ba8c0569b24d77c0e186937571fbdb
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-26 21:20:58 -0700

    add more fields to characterSheet html. Connect to firebase database.

--- Assets/Javascript/characterSheet.js
+++ Assets/Javascript/characterSheet.js
@@ -21,3 +21,39 @@ function fillClass(){
 }
 
 fillClass();
+
+
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyA7u4I8S7T1qOUo7iFhdSfW0qLx1xi3Mxg",
+authDomain: "ddproject-3a727.firebaseapp.com",
+databaseURL: "https://ddproject-3a727.firebaseio.com",
+projectId: "ddproject-3a727",
+storageBucket: "ddproject-3a727.appspot.com",
+messagingSenderId: "915757041211"
+};
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+// Initial Values
+var charName = "";
+var playerName = "";
+var level = 0;
+
+$("#submit-char-sheet").on("click", function(event) {
+    event.preventDefault();
+
+    charName = $("#char-name-input").val().trim();
+    playerName = $("#player-name-input").val().trim();
+    level = $("#level-input").val().trim();
+
+    // Code for the push
+    dataRef.ref().child("Characters").push({
+
+    charName: charName,
+    playerName: playerName,
+    level: level
+    });
+});
