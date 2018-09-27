65bf94589b28c13bafcf221f72a0a62065e4022c
Dardin <logandalec@gmail.com>
2018-09-26 19:30:02 -0700

    added new main page js, linked firebase to main

---
+++ Assets/Javascript/main.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function () {
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA7u4I8S7T1qOUo7iFhdSfW0qLx1xi3Mxg",
+    authDomain: "ddproject-3a727.firebaseapp.com",
+    databaseURL: "https://ddproject-3a727.firebaseio.com",
+    projectId: "ddproject-3a727",
+    storageBucket: "ddproject-3a727.appspot.com",
+    messagingSenderId: "915757041211"
+  };
+  firebase.initializeApp(config);
+
+
+})
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project1
-slack digest
+Game Master's Toolbox application
+
+This app is used to help a Game master organize character information for
+D&D 5e as well as utilize sound cloud to create custom atmospheric effects with instant 
+playback features.
