acae8e3a2d29f29ea907335d186a358da45046aa
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-10-02 22:54:39 -0700

    all character sheet info displays on main page

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -1,5 +1,5 @@
-$(document).ready( function () {
-      // Initialize Firebase
+$(document).ready(function () {
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyA7u4I8S7T1qOUo7iFhdSfW0qLx1xi3Mxg",
     authDomain: "ddproject-3a727.firebaseapp.com",
@@ -10,73 +10,67 @@ $(document).ready( function () {
   };
 
   firebase.initializeApp(config);
-  
+
   var dataref = firebase.database();
   var playref = dataref.ref().child('Characters');
   var enemyref = dataref.ref().child('Enemies');
   var npcref = dataref.ref().child('NPC')
-  //TODO add children
-  playref.on("child_added", function(snap) {
-    console.log("Working?" + snap.val().charName);
-    var charName = snap.val().charName;
-    var char = $("<a href='#'>").text(charName);
-    char.addClass('char-info');
-    char.attr('data-type', 'player');
+  
+  function retrieveCharacter(charInfo, dataType, element) {
+    var charEl = $("<div>").append($("<a href='#'>").text(charInfo.charName));
+    charEl.addClass("char-info");
+    charEl.data("type", dataType);
     // Output character info
-    $("#players").append(char);
+    element.append(charEl);
+
+    charEl.click(function (event) {
+      $("#char-info").empty();
+      $("#char-info-card").show();
+      $("#char-name").text(charInfo.charName + "'s ")
+      $("#player-name").text(charInfo.playName + ")");
+
+      var stats = [
+        "Class: " + charInfo.charClass,
+        "Level: " + charInfo.level,
+        "Race: " + charInfo.race,
+        "Subclass: " + charInfo.subclass,
+        "Experience Points: " + charInfo.ep,
+        "Health: " + charInfo.health,
+        "Speed: " + charInfo.speed,
+        "Armor Class: " + charInfo.armorClass,
+        "Background: " + charInfo.background,
+        "Alignment: " + charInfo.alignment,
+        "Strength: " + charInfo.strength,
+        "Dexterity: " + charInfo.dexterity,
+        "Constitution: " + charInfo.constitution,
+        "Intelligence: " + charInfo.intelligence,
+        "Wisdom: " + charInfo.wisdom,
+        "Charisma: " + charInfo.charisma,
+        "Skills: " + charInfo.skills,
+        "Saving Throws: " + charInfo.savingThrows,
+        "Traits: " + charInfo.traits,
+        "Proficiencies: " + charInfo.proficiencies,
+        "Ideals: " + charInfo.ideals,
+        "Bond: " + charInfo.bonds,
+        "Flaws: " + charInfo.flaws
+      ].forEach(stat => $("#char-info").append($("<p>").text(stat)));
+    });
+  }
+
+  // Player Characters
+  playref.on("child_added", function (snap) {
+    retrieveCharacter(snap.val(), "player", $("#players"));
   });
 
-  //
-  npcref.on("child_added", function(snap) {
-    console.log("Working?" + snap.val().charName);
-    var charName = snap.val().charName;
-    var char = $("<a href='#'>").text(charName);
-    char.addClass('char-info');
-    char.attr('data-type', 'npc');
-    // Output character info
-    $("#npcs").append(char);
+  // Non-player Characters
+  npcref.on("child_added", function (snap) {
+    retrieveCharacter(snap.val(), "npc", $("#npcs"));
   });
 
-  enemyref.on("child_added", function(snap) {
-    console.log("Working?" + snap.val().charName);
-    var charName = snap.val().charName;
-    var char = $("<a href='#'>").text(charName);
-    char.addClass('char-info');
-    char.attr('data-type', 'enemy');
-    // Output character info
-    $("#enemies").append(char);
+  //Enemies
+  enemyref.on("child_added", function (snap) {
+    retrieveCharacter(snap.val(), "enemy", $("#enemies"));
   });
 
-  //TODO: use this to display Character info!
-  //Use this.text() to search the database
-  // $('.char-info').on('click', function() {
-        //var type = $(this).attr(type);
-        //var name = $(this).text()
-        //var char = dataref.ref().child(type).child(name);
-        //do stuff with char on page
-  // })
-
-    // var charName = snap.val().charName;
-    // var playName = snap.val().playName;
-    // var charClass = snap.val().charClass;
-    // var level = snap.val().level;
-    // var race = snap.val().race;
-    // var subclass = snap.val().subclass;
-    // var ep = snap.val().ep;
-    // var background = snap.val().background;
-    // var alignment = snap.val().alignment;
-    // var strength = snap.val().strength;
-    // var dexterity = snap.val().dexterity;
-    // var constitution = snap.val().constitution;
-    // var intelligence = snap.val().intelligence;
-    // var wisdom = snap.val().wisdom;
-    // var charisma = snap.val().charisma;
-    // var skills = snap.val().skills;
-    // var savingThrows = snap.val().savingThrows;
-    // var traits = snap.val().traits;
-    // var proficiencies = snap.val().proficiencies;
-    // var ideals = snap.val().ideals;
-    // var bonds = snap.val().bonds;
-    // var flaws = snap.val().flaws;
-    // var ac = snap.val().ac;
+  $("#char-info-card").hide();
 })
