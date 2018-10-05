8fe7f63b790e346259fb019c438679414af69a8e
leannr78 <leann.roberts@gmail.com>
2018-10-05 15:46:19 -0700

    added video to accordion

--- Assets/Javascript/soundjs.js
+++ Assets/Javascript/soundjs.js
@@ -1,8 +1,6 @@
-
-
 $(document).on('ready', function () {
   var ajaxData;
-  //TODO: Add YouTube API configs
+  
   function buildQueryURL() {
     var queryURL = "https://www.googleapis.com/youtube/v3/search?";
     var queryParams = {
@@ -100,8 +98,6 @@ $(document).on('ready', function () {
   //   console.log('card id', $(this).attr('id'));
   // })
 
-
-
   // Function to empty out the videos
   function clear() {
     $("#video-section").empty();
@@ -153,11 +149,9 @@ $(document).on("click", ".list-group-item", function (event) {
   console.log('card id', $(this).attr('id'));
 })
 //Code for pushing to database 
-// ALDO HELPED HERE>>>>>>>>>>>>>
 var dataRef = firebase.database();
 $(document).on("click", ".addSongtoPlaylistbtn", function (event) {
   console.log(event.target.value, "attach a string to this thing")
-
   dataRef.ref().push(ajaxData.items[event.target.value])
   event.preventDefault();
   console.log("firebase push ", event);
@@ -170,58 +164,54 @@ $("#addToPlaylistbtn").on("click", function (event) {
   console.log(event);
   var userInput = $('#user-text').val()
   console.log(userInput)
-
-  var videoArray = [0];
-  //TODO: Input starter youtube id for the user that they can delete later 
-  //dataRef.ref().child("Playlists").child(userInput).set({songs: []}) //ARRON's
- // dataRef.ref().child("Playlists").child(userInput).push({songs:0});//ATTACK2
-  dataRef.ref().child("Playlists").child(userInput).set({videoArray});
-  //$("#playList1").text(userInput);
-
+  //Take user-text and add Playlist to database
+  dataRef.ref().child("Playlists").child(userInput).set({0:'blank'});
 });
 
-//TODO: create firebase event listener for user playlists
-// reference the childsnapshot data
-// dynamically create card that will live inside of the accordion
-// 
-database.ref().on("child_added", function(childSnapshot) {
+var x = 1; //playlist increment
+
+dataRef.ref().child("Playlists").on("child_added", function(childSnapshot) {
+  console.log("in dataref child");
+  //console.log(childSnapshot.val().child());
+  console.log(childSnapshot.key);
   console.log(childSnapshot.val());
+var playListName = childSnapshot.key
 
-  // Store everything into a variable.
-  var empName = childSnapshot.val().name;
-  var empRole = childSnapshot.val().role;
-  var empStart = childSnapshot.val().start;
-  var empRate = childSnapshot.val().rate;
-
-  // Employee Info
-  console.log(empName);
-  console.log(empRole);
-  console.log(empStart);
-  console.log(empRate);
-
-});
-/*
-function accordionPlaylist(){
-  return (`<div class="card">
+   $('.accordion').append(`<div class="card">
       <div class="card-header" id="headingOne">
       <h5 class="mb-0">'
       <button class="btn btn-link" id="playList1"
       type="button" data-toggle="collapse" 
-      data-target="#collapseOne" aria-expanded="true" 
+      data-target="#collapse${x}" aria-expanded="true" 
       aria-controls="collapseOne">
-         ${Playlist1}
+         ${playListName}
         </button>
         </h5>
-      </div>
-  
-      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      </div>`)
+//for each loop for songs in array
+for (var z=0; z<childSnapshot.val().length;z++){
+  var videoID = childSnapshot[z];
+  console.log(videoID);
+      $('.accordion').append(`  
+      <div id="collapse${x}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
       <div class="card-body">
-      ${VideoTitle} //Title/Name should be pulled from the database}
-      </div></div></div>`
+      <iframe class="ytplayer" type="text/html" width="200" height="100" 
+      src="https://www.youtube.com/embed/${videoID}" frameborder='0' allowfullscreen>
+      </div></div>`
    )
 }
-*/
+   $('.accordion').append(` <div class="row">
+   <div class="col-md-12">
+     <button id='modal-search-btn' class="btn btn-primary" data-target="#search-modal" data-toggle="modal">Search Music</button>
+   </div>
+ </div>
+ </div>`
+ )
+    x= x+1;
+  });
 
-$('#modal-search-btn').on('click', function () {
-  $('#search-modal').modal('toggle');
-})
+  $('#modal-search-btn').on('click', function () {
+    $('#search-modal').modal('toggle');
+   })
+
+
